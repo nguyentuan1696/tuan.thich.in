a713dd6ca7a111cc77bacba5b9c18d674e8af252
nguyentuan1696 <nguyentuan1696@gmail.com>
2023-07-30 16:13:34 +0700

    update text

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,7 @@
 <template>
   <h1 class="font-bold text-2xl mb-8 tracking-tighter">Tuan Nguyen</h1>
   <p>
-    Hi, I'm Tuan, a web developer, Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo officia
-    facilis autem vel quis dicta beatae incidunt qui voluptatibus assumenda?
+    Hi, I'm Tuan, a web developer
   </p>
   <section>
     <TheProject />
