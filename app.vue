910b71b7f040b6e4e257dff2055502669aba06ec
nguyentuan1696 <nguyentuan1696@gmail.com>
2023-07-31 22:33:29 +0700

    setup hotjar

--- app.vue
+++ app.vue
@@ -5,14 +5,14 @@
     </aside>
     <section>
       <NuxtPage />
-    
     </section>
   </main>
-        <TheFooter />
+  <TheFooter />
 </template>
 
 <script setup lang="ts">
 
+const hotJar = `(function(h,o,t,j,a,r){ h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}; h._hjSettings={hjid:3594748,hjsv:6}; a=o.getElementsByTagName('head')[0]; r=o.createElement('script');r.async=1; r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv; a.appendChild(r); })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
 useHead({
   bodyAttrs: {
     class:
@@ -21,8 +21,11 @@ useHead({
   htmlAttrs: {
     class: 'bg-white',
   },
- 
- 
+  script: [
+    {
+      innerHTML: process.env.NODE_ENV === 'development' ? '' : hotJar,
+    },
+  ],
 })
 useSeoMeta({
   title: 'Tuan Nguyen | tuan.thich.in',
