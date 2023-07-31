9a3fab16ed46ce8a07f0c2eba8c749fa3210e3e8
nguyentuan1696 <nguyentuan1696@gmail.com>
2023-07-31 22:26:53 +0700

    remove tag

--- app.vue
+++ app.vue
@@ -13,8 +13,6 @@
 
 <script setup lang="ts">
 
-const hotJar = `(function(h,o,t,j,a,r){ h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}; h._hjSettings={hjid:3594748,hjsv:6}; a=o.getElementsByTagName('head')[0]; r=o.createElement('script');r.async=1; r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv; a.appendChild(r); })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
-
 useHead({
   bodyAttrs: {
     class:
@@ -23,7 +21,7 @@ useHead({
   htmlAttrs: {
     class: 'bg-white',
   },
-  script: [{innerHTML: process.env.NODE_ENV === 'development' ? `${hotJar}`: '' }]
+ 
  
 })
 useSeoMeta({
