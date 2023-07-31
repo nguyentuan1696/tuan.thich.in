454cddd93b795acce3cb45f2d69b5daf56b0849b
nguyentuan1696 <nguyentuan1696@gmail.com>
2023-07-31 15:27:02 +0700

    update intro

--- components/TheIntro.vue
+++ components/TheIntro.vue
@@ -1,9 +1,16 @@
 <template>
-  <section>
+  <section class="flex flex-col items-start gap-x-1.5 gap-y-1.5">
     <h1 class="font-bold text-2xl mb-8 tracking-tighter">👋 Hey!</h1>
-    <p>
-      My name is Tuan Nguyen.<br />
-      I'm a web developer,
-    </p>
+        <p>My name is Tuan Nguyen.</p>
+        <p>I'm a web developer, focused on building accessible, human-centered products, I love working in small, highly collaborative teams facing complex business problems.</p>
+        <p>
+          Here's my email:
+          <NuxtLink
+            to="mailto:nguyentuan1696@gmail.com"
+            class="hover:underline hover:duration-300 cursor-pointer"
+            >nguyentuan1696@gmail.com</NuxtLink
+          >
+        </p>
+        <p>Don't be a stranger.</p>
   </section>
 </template>
