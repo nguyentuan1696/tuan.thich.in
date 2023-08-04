82be08cb33428c42a3725a9ca294098573b2032c
nguyentuan1696 <nguyentuan1696@gmail.com>
2023-08-04 21:18:43 +0700

    fix typo

--- components/TheIntro.vue
+++ components/TheIntro.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="flex flex-col items-start gap-x-1.5 gap-y-1.5">
-    <h1 class="font-bold text-2xl mb-8 tracking-tighter">👋 Hey!</h1>
+    <h1 class="font-bold text-2xl mb-8 tracking-tighter">Hi there 👋 </h1>
         <p>My name is Tuan Nguyen.</p>
         <p>I'm a web developer, focused on building accessible, human-centered products, I love working in small, highly collaborative teams facing complex business problems.</p>
         <p>
