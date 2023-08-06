c7909f793bbe2d5fe577d4b4dd4efa235e214481
nguyentuan1696 <nguyentuan1696@gmail.com>
2023-08-06 13:35:07 +0700

    using cloudflare worker

--- components/TheWidget.vue
+++ components/TheWidget.vue
@@ -1,20 +1,20 @@
 <template>
-  <section class="grid grid-cols-2 gap-4 gap-x-3.5 gap-y-3.5">
+  <section class="grid grid-cols-1 sm:grid-cols-2 gap-4 gap-x-3.5 gap-y-3.5">
     <div class="grid gap-x-3.5 gap-y-3.5">
       <div class="flex items-center gap-x-3.5 gap-y-3.5">
         <IconSpotify />
         <h2 class="font-bold text-xl tracking-tighter">Now Playing</h2>
       </div>
-      <NuxtLink :to="songUrl" target="_blank" rel="noopener">
+      <NuxtLink :to="item.songUrl" target="_blank" rel="noopener">
         <div
           class="flex items-center gap-x-3.5 gap-y-3.5 border rounded-2xl p-2"
         >
           <div class="w-1/4">
-            <nuxt-img :src="albumImageUrl" class="rounded-2xl" />
+            <!-- <nuxt-img :src="item.albumImageUrl" class="rounded-2xl" /> -->
           </div>
           <div>
-            <p class="font-semibold line-clamp-2">{{ title }}</p>
-            <p class="text-neutral-500">{{ artist }}</p>
+            <p class="font-semibold line-clamp-2">{{ item.title }}</p>
+            <p class="text-neutral-500 text-sm">{{ item.artist }}</p>
           </div>
         </div>
       </NuxtLink>
@@ -25,6 +25,7 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 interface APIBody {
   album: string
   albumImageUrl: string
@@ -33,9 +34,15 @@ interface APIBody {
   songUrl: string
   title: string
 }
-const { data } = await useFetch<APIBody>('/api/spotify')
-const albumImageUrl = data?.value?.albumImageUrl
-const title = data?.value?.title
-const artist = data?.value?.artist
-const songUrl = data?.value?.songUrl
+
+const item = ref({} as any)
+
+async function getData() {
+  const { data } = await useFetch<APIBody>('/api/spotify')
+  item.value = data.value
+}
+
+getData()
+
+
 </script>
