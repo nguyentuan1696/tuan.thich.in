882c5a19cf0cb285bc8d40aee23be7ae53238933
nguyentuan1696 <nguyentuan1696@gmail.com>
2026-01-11 19:25:45 +0700

    feat: add ThePartner component to homepage
    Introduce a new component to display partner information with links. The component includes a list of partners with their respective website links.

---
+++ components/ThePartner.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="flex flex-col gap-x-3.5 gap-y-3.5">
+    <h2 class="text-base">Partners</h2>
+    <ul class="list-disc pl-4 space-y-2">
+      <li v-for="(partner, index) in partners" :key="index">
+        <NuxtLink
+          :to="partner.link"
+          target="_blank"
+          rel="noopener"
+          class="text-gray-500"
+        >
+          {{ partner.title }}
+        </NuxtLink>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      partners: [
+        {
+          title: "BlueNet Tech & Services Co., Ltd",
+          link: "https://bluenet.vn",
+        },
+      ],
+    }
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <section class="space-y-10">
     <TheIntro />
     <TheWork />
+    <ThePartner />
     <TheTech />
   </section>
 </template>
